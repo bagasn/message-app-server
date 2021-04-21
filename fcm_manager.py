02e36590b8ae10d56f46600d5259a239476aac85
bagasn <bagasnasution@gmail.com>
2021-04-22 01:08:58 +0700

    new payload format

--- fcm_manager.py
+++ fcm_manager.py
@@ -4,25 +4,18 @@ from firebase_admin import credentials, messaging
 cred = credentials.Certificate("serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
 
-single_token = 'dK9g78gyTNGJUbW5zaILb5:APA91bGxdD-EFQOJhUpJEGwTao3rBPyo3E' \
-               '-47h5lyVO34uuNrg_P6IWIOI91NvP16MsOEZsh73QIe5DVl5o09CSpXM16' \
-               'XloGe_6Cli3toQb6iXT8yt97mFATwTGXOgbcqVybHB5SLslT'
+single_token = 'dK9g78gyTNGJUbW5zaILb5:APA91bGxdD-EFQOJhUpJEGwTao3rBPyo3E-47h' \
+               '5lyVO34uuNrg_P6IWIOI91NvP16MsOEZsh73QIe5DVl5o09CSpXM16XloGe_6' \
+               'Cli3toQb6iXT8yt97mFATwTGXOgbcqVybHB5SLslT'
 
 
 def send_push():
     # See documentation on defining a message payload.
     message = messaging.Message(
         token=single_token,
-        notification=messaging.Notification(
-            title='[TEST] New Order',
-            body='You have new order, Open Application BASSS to manage!'
-        ),
-        android=messaging.AndroidConfig(
-            notification=messaging.AndroidNotification(
-                click_action='OPEN_ACTIVITY_SECOND',
-            ),
-        ),
         data={
+            'title': '[TEST] New Order',
+            'body': 'You have new order',
             'orderId': '1234',
             'orderStatus': 'newOrder',
             'category': 'orderStatus'
